dbf4b7712785226903921a1c9dd7c93aedee0ae8
jlgdev <gdurand@jlgarchitects.com>
2021-11-17 11:18:17 -0600

    updated

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Hello Universe!')
+// typescript only helps during runtime; before code is compiled
+function add(n1: number, n2: number, showResult: boolean, phase: string) {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.4;
+
+const result = add(number1, number2, true, "Test");
+
+console.log(result);
